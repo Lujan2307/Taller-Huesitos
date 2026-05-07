1969f8527b92adc8dd24cc4a1350b4fb854c7e9a
cusnico <nicocoloradozab@gmail.com>
2026-05-07 09:13:36 -0500

    Sistema de dialogo
    Yupi

---
+++ Taller Huesitos/Assets/_Main/Scripts/Switch.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using TMPro;
+
+
+
+public class Switch : MonoBehaviour
+{
+    [SerializeField] public TextMeshProUGUI textoResultante;
+
+    void Start()
+    {
+       
+        EstadosDelJuego(3);
+    }
+    void Update()
+    {
+        
+    }
+
+    public void EstadosDelJuego(int numero)
+    {
+        switch (numero)
+        {
+        case 0:
+                textoResultante.text = "Me inclino hacia el, lo abrazo y lo beso.";
+
+                break;
+
+        case 1:
+                textoResultante.text = "Retrocedo unos pasos, tomo impulso, corro y, de una patada, lo mando a volar.";
+
+                break;
+
+        case 2:
+                textoResultante.text = "Me pongo tímide, uwu, y echo a correr, ñya.";
+
+                break;
+
+        default:
+                textoResultante.text = "Te encontraste a tu Crush, ¿qué vas a hacer?";
+
+                break;
+
+        }
+    }
+}
