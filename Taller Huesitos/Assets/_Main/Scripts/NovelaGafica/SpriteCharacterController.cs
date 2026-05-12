2fdfab19f7c2f180e949d5c8727277ac14a986b2
Lujan2307 <mlujan.2325@gmail.com>
2026-05-12 08:55:49 -0500

    Asignacion de variables y Funciones nuevas

---
+++ Taller Huesitos/Assets/_Main/Scripts/NovelaGafica/SpriteCharacterController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class SpriteCharacterController : MonoBehaviour
+{
+    [Header("Array SpriteRenderer")]
+    [SerializeField]
+    private SpriteRenderer[] _spriteRenderers;
+
+    [Header("Sprite")]
+    [SerializeField]
+    private Sprite _mainSprite;
+    [SerializeField]
+    private Sprite _hairSprite;
+    [SerializeField]
+    private Sprite _whiteSprite;
+
+    [Header("Array Sprite")]
+    [SerializeField]
+    private Sprite[] _tearSprites;
+    [SerializeField]
+    private Sprite[] _mouthSprites;
+    [SerializeField]
+    private Sprite[] _graphicSprites;
+    [SerializeField]
+    private Sprite[] _eyelibsSprites;
+    [SerializeField]
+    private Sprite[] _pupilsSprites;
+    [SerializeField]
+    private Sprite[] _eyebrowsSprites;
+    [SerializeField]
+    private Sprite[] _cheeksSprites;
+
+}
