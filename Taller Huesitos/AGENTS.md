1cf44c5161231ed65832dec81bc120bcde983bae
cusnico <nicocoloradozab@gmail.com>
2026-05-14 11:44:49 -0500

    Leona solucionadora 3
    Auida

--- Taller Huesitos/AGENTS.md
+++ Taller Huesitos/AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: Taller Huesitos
 - Unity version: Unity 6000.3.10f1
 - Active game object:
-  - Name: Character (1)
+  - Name: Switch
   - Tag: Untagged
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Taller Huesitos/Assets/_Main/Scripts/Switch.cs
+++ Taller Huesitos/Assets/_Main/Scripts/Switch.cs
@@ -9,34 +9,34 @@ public class Switch : MonoBehaviour
 
     void Start()
     {
-       
+
         EstadosDelJuego(3);
     }
     void Update()
     {
-        
+
     }
 
     public void EstadosDelJuego(int numero)
     {
         switch (numero)
         {
-        case 0:
+            case 0:
                 textoResultante.text = "Me inclino hacia el, lo abrazo y lo beso.";
 
                 break;
 
-        case 1:
+            case 1:
                 textoResultante.text = "Retrocedo unos pasos, tomo impulso, corro y, de una patada, lo mando a volar.";
 
                 break;
 
-        case 2:
+            case 2:
                 textoResultante.text = "Me pongo tímide, uwu, y echo a correr, ñya.";
 
                 break;
 
-        default:
+            default:
                 textoResultante.text = "Te encontraste a tu Crush, ¿qué vas a hacer?";
 
                 break;
